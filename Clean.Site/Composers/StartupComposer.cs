0050de6094151eee4621788c7df86ecc12db6a50
Vladyslav Bezpalko <60620161+vladbezp@users.noreply.github.com>
2023-10-20 14:06:06 +0300

    added block-preview

---
+++ Clean.Site/Composers/StartupComposer.cs
@@ -0,0 +1,16 @@
+﻿using Clean.Site.NotificationHandlers;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Notifications;
+
+namespace Clean.Site.Composing
+{
+    internal class StartupComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder
+                .AddNotificationHandler<ServerVariablesParsingNotification,
+                    ServerVariablesParsingNotificationHandler>();
+        }
+    }
+}
